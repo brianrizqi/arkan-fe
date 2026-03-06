de694938528b50c3587d4409943531cbaa8f64d5
Brian Rizqi <rizqibrian27@gmail.com>
2026-03-07 05:52:02 +0700

    feat: Add new Sustainability page and configure it in Vite build.

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
                 portfolio: resolve(__dirname, 'portfolio.html'),
                 'journal-detail': resolve(__dirname, 'journal-detail.html'),
                 'portfolio-detail': resolve(__dirname, 'portfolio-detail.html'),
+            'sustainability': resolve(__dirname, 'sustainability.html'),
             }
         }
     },
